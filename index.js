aaeee06150d7e7611577e908c706f8c5d0260f0b
Cole Willsea <coleww@gmail.com>
2014-10-14 07:57:07 -0700

    handling pentatonic scales and multi-octave things

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-module.exports = int2freq;
+module.exports = int2freq
 
 var scales = {
   major: [2, 2, 1, 2, 2, 2, 1],
-  minor: [2, 1, 2, 2, 1, 2, 2]
-};
+  minor: [2, 1, 2, 2, 1, 2, 2],
+  pentMaj: [2, 2, 3, 2, 3],
+  pentMin: [3, 2, 2, 3, 2]
+}
 
 var notes = [
   'A0', 'A#0', 'B0', 'C1', 'C#1', 'D1', 'D#1', 'E1', 'F1', 'F#1', 'G1', 'G#1',
@@ -14,7 +16,7 @@ var notes = [
   'A5', 'A#5', 'B5', 'C6', 'C#6', 'D6', 'D#6', 'E6', 'F6', 'F#6', 'G6', 'G#6',
   'A6', 'A#6', 'B6', 'C7', 'C#7', 'D7', 'D#7', 'E7', 'F7', 'F#7', 'G7', 'G#7',
   'A7', 'A#7', 'B7', 'C8'
-];
+]
 
 var str2freq = {
   'A0' :27.5000,
@@ -105,22 +107,29 @@ var str2freq = {
   'A#7' :3729.31,
   'B7' :3951.07,
   'C8' :4186.01
-};
+}
 
 
 function int2freq(options, intNote){
-  var ind = notes.indexOf(options.tonic);
-  var scale = scales[options.scale];
+
+  var ind = notes.indexOf(options.tonic)
+  // if(ind === -1) throw 'what is up with that tonic?'
+  var scale = scales[options.scale]
+  // if(!scale) throw 'what is up with that scale?'
+
+  while (Math.abs(intNote) > scale.length){
+    scale = scale.concat(scale)
+  }
 
   if(intNote >= 0){
     for (var i = 0; i < intNote; i+= 1){
-      ind += scale[i];
+      ind += scale[i]
     }
   } else {
     for (var j = -1; j >= intNote; j-= 1){
-      ind -= scale[scale.length + j];
+      ind -= scale[scale.length + j]
     }
   }
-  return str2freq[notes[ind]];
+  return str2freq[notes[ind]]
 
 }
--- test.js
+++ test.js
@@ -16,13 +16,21 @@ test('goes negative', function(t){
   t.equal(int2freq({tonic: 'C3', scale: 'major'}, -2), 110.000);
 });
 
+test('does other scales', function(t){
+  t.plan(2);
+  t.equal(int2freq({tonic: 'A4', scale: 'pentMaj'}, -3), 277.183);
+  t.equal(int2freq({tonic: 'A4', scale: 'pentMin'}, 3), 659.255);
+});
 
+test('allows for range of A0-C8', function(t){
+  t.plan(2);
+  t.equal(int2freq({tonic: 'A7', scale: 'minor'}, 2), 4186.01);
+  t.equal(int2freq({tonic: 'B0', scale: 'minor'}, -1), 27.5000);
+});
 
+test('complains about out of range inputs', function(t){
+  t.plan(2);
+  t.equal(int2freq({tonic: 'A0', scale: 'pentMaj'}, -1), undefined);
+  t.equal(int2freq({tonic: 'C8', scale: 'minor'}, 1), undefined);
+});
 
-
-
-
-
-  // t.equal(int2freq({tonic: 'A4', scale: 'pentatonic'}, 0), 440.000);
-  // t.equal(int2freq({tonic: 'A4', scale: 'minor'}, 1), 493.883);
-  // t.equal(int2freq({tonic: 'C3', scale: 'major'}, -2), 110.000);
